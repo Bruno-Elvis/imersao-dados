5310eebcc57750be873f03656a8eb950d453b338
Bruno-Elvis <brunoelvis@outlook.com.br>
2021-05-03 21:13:35 -0300

    Desafios_Concluidos

--- leituraeTratamento.py
+++ leituraeTratamento.py
@@ -1,9 +1,11 @@
 from numpy.core.fromnumeric import shape
 import pandas as pd
 
-url_dados_origem = 'https://github.com/alura-cursos/imersaodados3/blob/main/dados/dados_experimentos.zip?raw=true'
+url_dados_origem = 'https://github.com/Bruno-Elvis/imersaoDados/blob/master/dados/dados_experimentos.zip?raw=true'
+dados_origem_local = 'E:\Arquivos\LEARNS\Python\ImersaoDados\dados\dados_experimentos.csv'
 
-dados = pd.read_csv(url_dados_origem, compression = 'zip')
+#dados = pd.read_csv(url_dados_origem, compression = 'zip')
+dados = pd.read_csv(dados_origem_local)
 
 #print(dados) #Lê os dados completos da origem de dados
 
@@ -13,7 +15,7 @@ print(dados.tail()) #Retorna as 5 últimas linhas incluindo o cabeçalho para vi
 print()
 print(shape(dados))
 print()
-print()
+
 print("Controle da classe 'tratamento': ", dados['tratamento'].unique())
 print("Controle da classe 'tempo': ", dados['tempo'].unique())
 print("Controle da classe 'dose': ", dados['dose'].unique())
@@ -23,3 +25,8 @@ print("Controle da classe 'g-0': ", dados['g-0'].unique()) #Valores relacionados
 print()
 
 print("Quantidade de registros (frequência) por classificação:\n", dados['tratamento'].value_counts(), "\nNúmero total de registros da Classe: ", dados['tratamento'].count())
+print()
+print("Proporção da classe 'tratamento':\n", dados['tratamento'].value_counts(normalize=True) * 100)
+print()
+print(shape(dados['droga'].unique()))
+print(dados['droga'].value_counts())
